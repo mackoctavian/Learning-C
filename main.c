640083d746e61f026bfe5d1b10990a6c63f14ad1
Mack Octavian <maestrooctavian@gmail.com>
2023-04-01 23:28:24 +0300

    First commit of the learning C project

---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+ * Author: Mack Octavian
+ * Project: Learning C
+ */
+#include "main.h"
+/**
+ * main - Entry point of the program
+ */
+int main()
+{
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * Desc: Header file containing prototype of all functions
+ *       Used in the project
+ */
+
+#endif
