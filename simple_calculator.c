b86f88166da8a6aaefa3e25543f01731d7bdc1d1
Mack Octavian <maestrooctavian@gmail.com>
2023-04-02 00:07:57 +0300

    Added a simple calculator program

--- simple_calculator.c
+++ simple_calculator.c
@@ -2,3 +2,42 @@
  * A program to create a simpe calcuator
  * Author: Mack Octavian
  */
+#include <stdio.h>
+
+int simpleCalculator()
+{
+    char operation;
+    double n1, n2;
+
+    printf("Enter an operator(+,-,*,/): ");
+    scanf("%c", operation);
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &n1, &n2);
+
+    switch (operation)
+    {
+    case '+':
+        printf("%lf + %lf = %lf", n1, n2, n1 + n2);
+        break;
+
+    case '-':
+        printf("%lf - %lf = %lf", n1, n2, n1 - n2);
+        break;
+
+    case '*':
+        printf("%lf * %lf = %lf", n1, n2, n1 * n2);
+        break;
+
+    case '/':
+        printf("%lf / %lf = %lf", n1, n2, n1 / n2);
+        break;
+
+    /**
+     * When Operation does not match any case
+     */
+    default:
+        printf("Error! Operator is not correct.");
+        break;
+    }
+    return (0);
+}
